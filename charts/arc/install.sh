0188bfb47b5dd10fa3eb9bc179cdd4b1d0f6c734
Aethen22 <Aethen2@gmail.com>
2024-09-22 23:54:18 +0200

    feat(arc): added a script to run an installation or upgrade on an arc installation

--- charts/arc/install.sh
+++ charts/arc/install.sh
@@ -5,7 +5,46 @@ decryptSecretsYaml () {
     sops -d secrets.enc.yaml | yq '.data' | base64 -d > secrets.yaml
 }
 
-decryptSecretsYaml
+encryptSecretsYaml () {
+    base64Secrets="data: \""
+    base64Secrets+="$(cat secrets.yaml | base64 -w 0)"
+    base64Secrets+=\"
+    echo -n $base64Secrets > secrets.b64.yaml
+    sops encrypt secrets.b64.yaml > secrets.enc.yaml
+    rm -rf secrets.b64.yaml
+}
+
+if [ -f ./secrets.enc.yaml ]; then
+  INSTALL=false
+  echo "Decrypting secrets.enc.yaml"
+  decryptSecretsYaml
+else
+  INSTALL=true
+  echo "secrets.enc.yaml file not found"
+  echo "You will be prompted for the required value to fill the secrets.enc.yaml file"
+  echo "ARC will be deployed to the cluster you're connected to"
+  echo "Input the Namespace where you will install ARC:"
+  read NAMESPACE
+  NAMESPACE="'.namespace= \"$NAMESPACE\"'"
+  /bin/bash -c  "yq -n $NAMESPACE" > secrets.yaml
+  echo "Input your Github Application ID:"
+  read GITHUB_APPID
+  GITHUB_APPID="'.github_application_id= \"$GITHUB_APPID\"'"
+  /bin/bash -c  "yq -n $GITHUB_APPID" >> secrets.yaml
+  echo "Input your Github Application Installation ID:"
+  read GITHUB_INSTALLATION_ID
+  GITHUB_INSTALLATION_ID="'.github_application_instalation_id= \"$GITHUB_INSTALLATION_ID\"'"
+  /bin/bash -c  "yq -n $GITHUB_INSTALLATION_ID" >> secrets.yaml
+  echo "Input your Github Application private key:"
+  read GITHUB_APP_PRIVATE_KEY
+  GITHUB_APP_PRIVATE_KEY="'.github_application_private_key= \"$GITHUB_APP_PRIVATE_KEY\"'"
+  /bin/bash -c  "yq -n $GITHUB_APP_PRIVATE_KEY" >> secrets.yaml
+  echo "Input your organization:"
+  read ORG
+  ORG="'.organization= \"$ORG\"'"
+  /bin/bash -c  "yq -n $ORG" >> secrets.yaml
+  encryptSecretsYaml
+fi
 
 # Get values from secrets.yaml
 NAMESPACE=`yq '.namespace' secrets.yaml`
@@ -21,17 +60,34 @@ kubectl create secret generic arc-gh-secret \
     --from-literal=github_app_installation_id="${GITHUB_INSTALLATION_ID}" \
     --from-literal=github_app_private_key="$(GITHUB_APP_PRIVATE_KEY)"
 
-# Install arc controller
-helm install arc \
-    --namespace "${NAMESPACE}" \
-    --create-namespace \
-    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller \
-    -f controller.values.yaml
+if [ "$INSTALL" = "true" ]; then
+  # Install arc controller
+  helm upgrade arc \
+      --namespace "${NAMESPACE}" \
+      --create-namespace \
+      oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller \
+      -f controller.values.yaml
 
-# Install arc runner
-helm install arc-runner-set \
-    --namespace "${NAMESPACE}" \
-    --create-namespace \
-    --set githubConfigUrl="https://github.com/${ORG}" \
-    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set \
-    -f scaleset.values.yaml
+  # Install arc runner
+  helm upgrade arc-runner-set \
+      --namespace "${NAMESPACE}" \
+      --create-namespace \
+      --set githubConfigUrl="https://github.com/${ORG}" \
+      oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set \
+      -f scaleset.values.yaml
+else
+  # Upgrade arc controller
+  helm upgrade arc \
+      --namespace "${NAMESPACE}" \
+      --create-namespace \
+      oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller \
+      -f controller.values.yaml
+
+  # Upgrade arc runner
+  helm upgrade arc-runner-set \
+      --namespace "${NAMESPACE}" \
+      --create-namespace \
+      --set githubConfigUrl="https://github.com/${ORG}" \
+      oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set \
+      -f scaleset.values.yaml
+fi
