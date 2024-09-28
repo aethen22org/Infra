1ba5dd09fd4b0802b368589b8b7d952b232c48bd
Aethen22 <Aethen2@gmail.com>
2024-09-28 14:36:31 +0200

    chore(rm): removed scritps as they are no longer used

--- images/agent/scripts/setuphelm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Setup brew env's so we can use helm, then add the registry and get it's contents
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-helm repo add helm-registry http://helm-registry.helm-registry.svc.cluster.local:8080/charts
-helm repo update
